43eb86760d02ba2d6a384565c82b5c2d543560f1
Zack Hartmann <zackhartmann@gmail.com>
2017-08-16 15:21:08 -0400

    Aesthetic Done - Logic in Process

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
 var human = "X";
 var computer = "O";
+var exigents =	[
+								[1, 2, 3],
+								[4, 5, 6],
+								[7, 8, 9],
+								[1, 4, 7],
+								[2, 5, 8],
+								[3, 6, 9],
+								[1, 5, 9],
+								[7, 5, 3]
+								];
+var emptyBoxes = [1,2,3,4,5,6,7,8,9];
 
-function flipBox(box) {
+function testFlip(box) {
+	emptyBoxes.splice(2,2);
+	var randomBox = emptyBoxes[Math.floor(Math.random()*emptyBoxes.length)];
+	// remove chosen box from emptyBoxes
+	var rmIndex = emptyBoxes.indexOf[randomBox];
+	emptyBoxes.splice(rmIndex,rmIndex);
+	console.log(emptyBoxes);
+}
+
+
+function tap(box) {
+	testFlip(box);
+	// manFlip(box);
+}
+
+function manFlip(box) {
 	if (document.getElementById(box).innerHTML.length === 0) {
 		document.getElementById(box).innerHTML = human;
 		document.getElementById(box).style.backgroundColor = '#ff8080';
-		goAI();
+		machineFlip();
 	}
 }
 
-function goAI() {
+function machineFlip() {
+	testGame();
 	var box = Math.floor((Math.random() * 9) + 1);
-	if (document.getElementById(box).innerHTML.length === 0) {
+	if (document.getElementById(box).innerHTML === "") {
 		document.getElementById(box).innerHTML = computer;
 		document.getElementById(box).style.backgroundColor = '#8080ff';
 	} else {
-		goAI();
+		machineFlip();
 	}
+}
+
+function testGame() {
+	var winner = human; // "X"
+	var success = true;
+	// loops through array containing 8 arrays of 3-box win combos
+	exigents.forEach(function(threeBoxes) {
+		// for each inner array loops through 3 boxes to test if they agree
+		threeBoxes.forEach(function(box) {
+			if (document.getElementById(box).innerHTML !== winner) {
+				success = false;
+			}
+		});
+	});
+	console.log(success);
 }
