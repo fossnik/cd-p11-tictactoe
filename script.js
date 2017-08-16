84be86e3b530604d6e4d6c74ab2e000949f2a268
Zack Hartmann <zackhartmann@gmail.com>
2017-08-16 17:59:19 -0400

    Show Boxes

--- script.js
+++ script.js
@@ -49,14 +49,16 @@ function testGame(player) {
 	// loops through array containing 8 arrays of 3-box win combos
 	exigents.forEach(function(consecutiveBoxes) {
 		// for each inner array loops through 3 boxes to test if they agree
-		var success = true;
+		var success = 0;
 		consecutiveBoxes.forEach(function(box) {
-			if (document.getElementById(box).innerHTML !== player) {
-				success = false;
+			if (document.getElementById(box).innerHTML === player) {
+				success += 1;
 			}
 		});
-		if (success) {
+		if (success >= 3) {
 			gameOver(player);
+		} else if (player === human & success >= 2) {
+			blockMoveAI(consecutiveBoxes);
 		}
 	});
 }
@@ -66,4 +68,8 @@ function gameOver(player) {
 	winHTML += "<br><div><button class='btn btn-success'";
 	winHTML += "onclick='window.location.reload()'>New Game</button></div>";
 	document.getElementById("tictactoe").innerHTML = winHTML;
+}
+
+function blockMoveAI(consecutiveBoxes) {
+	console.log(consecutiveBoxes);
 }
